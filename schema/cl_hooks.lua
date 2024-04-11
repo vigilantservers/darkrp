0e3ede9f8f1bf22654e19747b1a302a04a4d3d2f
Tom.Hewison <trhewison@gmail.com>
2024-04-11 17:07:32 +0100

    started to add banking etc...

--- schema/cl_hooks.lua
+++ schema/cl_hooks.lua
@@ -5,3 +5,11 @@
 function Schema:CharacterLoaded(character)
 	self:ExampleFunction("@serverWelcome", character:GetName())
 end
+
+hook.Add("BankBalanceChanged", "NotifyPlayerOfBankChange", function(char, oldBalance, change, newBalance)
+   if(change > 0) then
+      char:Notify("You have received $"..change.." in your bank account.")
+   else
+      char:Notify("You have withdrawn $"..math.abs(change).." from your bank account.")
+   end
+end)
